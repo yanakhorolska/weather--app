ceba29d78dcec862eca41065cd4ace1257b66881
yanakhorolska <yana.khorolska@protonmail.com>
2023-08-04 13:32:31 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(false);
 
   const apiKey = "0c0610d7593923374fcbd2bdb3babb71";
   const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
@@ -18,8 +20,10 @@ const App = () => {
         },
       });
       setWeatherData(response.data);
+      setError(false);
     } catch (error) {
-      console.error("Error fetching weather data:", error);
+      setError(true);
+      setWeatherData(null);
     }
   };
 
@@ -33,25 +37,37 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter city name"
-          value={city}
-          onChange={handleCityChange}
-        />
-        <button type="submit">Get Weather</button>
-      </form>
+    <div className="fon">
+      <div className="blur">
+        <div className="box">
+          <h1 className="header">Weather in your city</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter city name"
+              value={city}
+              onChange={handleCityChange}
+            />
+            <button className="button" type="submit">
+              Get Weather
+            </button>
+          </form>
 
-      {weatherData && (
-        <div>
-          <h2>{weatherData.name}</h2>
-          <p>Temperature : {weatherData.main.temp}°C</p>
-          <p>Description : {weatherData.weather[0].description}</p>
+          {weatherData && (
+            <div>
+              <h2>{weatherData.name}</h2>
+              <p>Temperature : {weatherData.main.temp}°C</p>
+              <p>Description : {weatherData.weather[0].description}</p>
+            </div>
+          )}
+          {error && (
+            <div>
+              <h2>Please, enter the correct city name</h2>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
